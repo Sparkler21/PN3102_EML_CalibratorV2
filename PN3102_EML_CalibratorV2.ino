1ff9eae5a7f38bdfcd76dd99cedceef1dd0fc4ef
Sparkler21 <mark@emltd.net>
2025-08-14 13:12:16 +0100

    First Commit
    Signed-off-by: Sparkler21 <mark@emltd.net>

---
+++ PN3102_EML_CalibratorV2.ino
@@ -0,0 +1,58 @@
+/*
+ * Portenta Machine Control - Analog out
+ *
+ * This example demonstrates the utilization of the Analog out channels on the Machine Control.
+ * The example configures the channels' PWM period in the setup and then iterates through voltage
+ * output values from 0V to 10.5V in a loop.
+ *
+ * The circuit:
+ *  - Portenta H7
+ *  - Portenta Machine Control
+ *
+ * This example code is in the public domain. 
+ * Copyright (c) 2024 Arduino
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+#include <Arduino_PortentaMachineControl.h>
+
+#define PERIOD_MS 4 /* 4ms - 250Hz */
+
+int needleValve0 = 0;
+int needleValve1 = 0;
+int needleValve2 = 0;
+int needleValve3 = 0;
+
+float voltage = 0;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect.
+  }
+
+  MachineControl_AnalogOut.begin();
+
+  MachineControl_AnalogOut.setPeriod(needleValve0, PERIOD_MS); 
+  MachineControl_AnalogOut.setPeriod(needleValve1, PERIOD_MS);
+  MachineControl_AnalogOut.setPeriod(needleValve2, PERIOD_MS);
+  MachineControl_AnalogOut.setPeriod(needleValve3, PERIOD_MS);
+}
+
+void loop() {
+  MachineControl_AnalogOut.write(needleValve0, voltage);
+  MachineControl_AnalogOut.write(needleValve1, voltage);
+  MachineControl_AnalogOut.write(needleValve2, voltage);
+  MachineControl_AnalogOut.write(needleValve3, voltage);
+
+  Serial.println("All channels set at " + String(voltage) + "V");
+  
+  voltage = voltage + 0.1;
+  /* Maximum output value is 10.5V */
+  if (voltage >= 10.5) {
+    voltage = 0;
+    delay(100); /* Additional 100 ms delay introduced to manage 10.5V -> 0V fall time of 150 ms */
+  }
+
+  delay(100);
+}
