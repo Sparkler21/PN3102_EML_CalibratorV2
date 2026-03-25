ad10eb7ae8a02ec5fc7ae29fd91ab2441eef5898
Sparkler21 <mark@emltd.net>
2026-03-25 16:01:55 +0000

    Cleaned up the code and format.
    Signed-off-by: Sparkler21 <mark@emltd.net>

--- PN3102_EML_CalibratorV2.ino
+++ PN3102_EML_CalibratorV2.ino
@@ -6,78 +6,27 @@
 //*****************************************************************************************************
 #define PERIOD_MS 4        // 4 ms = 250 Hz
 
-// Solenoid switches (digital out) for on/off supply of water to the water containers
-// NOTE: connect pin "24V IN" of the DIGITAL_OUTPUTS connector to 24V
-#define SOLENOID_1 0
-//#define SOLENOID_2 1
-//#define SOLENOID_3 2
-//#define SOLENOID_4 3
-
-// Needle Valves (analog out) for control of water flow
-#define NEEDLE_VALVE_1 0
-//#define NEEDLE_VALVE_2 1
-//#define NEEDLE_VALVE_3 2
-//#define NEEDLE_VALVE_4 3
-
 //*****************************************************************************************************
 //GLOBALS
 //*****************************************************************************************************
+
+//*****************************************************************************************************
+//Core Enums
+//*****************************************************************************************************
 // Define operation modes
 enum OperationMode {
-  MODE_IDLE = 1,
+  MODE_IDLE = 0,
   MODE_SETUP,
   MODE_BALANCE,
   MODE_TEST,
   MODE_ERROR,
   MODE_SHUTDOWN
 };
-
-static const uint8_t XIAO_ADDR = 0x2A;
-
 OperationMode mode = MODE_IDLE;
 
 //Load Cell Globals
+static const uint8_t XIAO_ADDR = 0x2A;
 float lcWeight[4] = {0};
-//SPISettings adsSpiSettings(1000000, MSBFIRST, SPI_MODE1);
-
-//  Voltage levels (0-10V) to control water flow / needlevalves
-float voltageNV[4] = {0};
-
-// Raingauge channel counts
-unsigned long pulseCountRG1A = 0;   // total pulses counted on RG1 chA
-unsigned long pulseCountRG1B = 0;   // total pulses counted on RG1 chB
-//unsigned long pulseCountRG2A = 0;   // total pulses counted on RG2 chA
-//unsigned long pulseCountRG2B = 0;   // total pulses counted on RG2 chB
-//unsigned long pulseCountRG3A = 0;   // total pulses counted on RG3 chA
-//unsigned long pulseCountRG3B = 0;   // total pulses counted on RG3 chB
-//unsigned long pulseCountRG4A = 0;   // total pulses counted on RG4 chA
-//unsigned long pulseCountRG4B = 0;   // total pulses counted on RG4 chB
-// Raingauge channel last states
-bool lastPulseStateRG1A = LOW;     // last pin state for edge detection on RG1 chA
-bool lastPulseStateRG1B = LOW;     // last pin state for edge detection on RG1 chB
-//bool lastPulseStateRG2A = LOW;     // last pin state for edge detection on RG2 chA
-//bool lastPulseStateRG2B = LOW;     // last pin state for edge detection on RG2 chB
-//bool lastPulseStateRG3A = LOW;     // last pin state for edge detection on RG3 chA
-//bool lastPulseStateRG3B = LOW;     // last pin state for edge detection on RG3 chB
-//bool lastPulseStateRG4A = LOW;     // last pin state for edge detection on RG4 chA
-//bool lastPulseStateRG4B = LOW;     // last pin state for edge detection on RG4 chB
-// Raingauge channel current states
-bool currentPulseStateRG1A = LOW;  // last pin state for edge detection on RG1 chA
-bool currentPulseStateRG1B = LOW;  // last pin state for edge detection on RG1 chb
-//bool currentPulseStateRG2A = LOW;  // last pin state for edge detection on RG2 chA
-//bool currentPulseStateRG2B = LOW;  // last pin state for edge detection on RG2 chB
-//bool currentPulseStateRG3A = LOW;  // last pin state for edge detection on RG3 chA
-//bool currentPulseStateRG3B = LOW;  // last pin state for edge detection on RG3 chB
-//bool currentPulseStateRG4A = LOW;  // last pin state for edge detection on RG4 chA
-//bool currentPulseStateRG4B = LOW;  // last pin state for edge detection on RG4 chB
-
-// Serial watchdog
-bool serialActive = false;
-unsigned long lastSerialCheck = 0;
-
-// Heartbeat LED
-unsigned long lastHeartbeat = 0;
-bool heartbeatState = false;
 
 struct __attribute__((packed)) WeightPacket {
   float lc1;
@@ -87,22 +36,54 @@ struct __attribute__((packed)) WeightPacket {
   uint32_t sequence;
 };
 
+//  Voltage levels (0-10V) to control water flow / needlevalves
+float voltageNV[4] = {0};
+
+// Raingauge channel counts
+unsigned long pulseCountRGA[4] = {0};   // total pulses counted on RG chA
+unsigned long pulseCountRGB[4] = {0};   // total pulses counted on RG chB
+// Raingauge channel last states
+bool lastPulseStateRGA[4] = {LOW};     // last pin state for edge detection on RG chA
+bool lastPulseStateRGB[4] = {LOW};     // last pin state for edge detection on RG chB
+// Raingauge channel current states
+bool currentPulseStateRGA[4] = {LOW};  // last pin state for edge detection on RG chA
+bool currentPulseStateRGB[4] = {LOW};  // last pin state for edge detection on RG chb
+
+// Serial watchdog
+bool serialActive = false;
+unsigned long lastSerialCheck = 0;
+
+// Heartbeat LED
+unsigned long lastHeartbeat = 0;
+bool heartbeatState = false;
+
 //*****************************************************************************************************
 //HELPERS
 //*****************************************************************************************************
 
-//****************************
-//Solenoids for control of
-//filling water tanks
-//****************************
+//***********************************************************
+// Main Menu
+//***********************************************************
+void printMenu() {
+  Serial.println("\nEnter a number to choose a mode:");
+  Serial.println("0. IDLE / Exit current mode");
+  Serial.println("1. SETUP");
+  Serial.println("2. BALANCE");
+  Serial.println("3. TEST");
+  Serial.println("4. ERROR");
+  Serial.println("5. SHUTDOWN");
+}
+
+//***********************************************************
+// Device - Solenoids / Tank Fill
+//***********************************************************
 void solenoid_controller(uint8_t solenoid_no, PinStatus status){  //  Turns the solenoids On (HIGH) or Off (LOW)
   MachineControl_DigitalOutputs.write(solenoid_no, status);
 }
 
-//****************************
-//NeedleValves for delivering
-//accurate flow to gauges
-//****************************
+//***********************************************************
+// Device - Needle Valves / Flow Control
+//***********************************************************
 void needleValve_controller(int needleValveNo){
 
   //Set the values of the voltages for the four needleValves??????
@@ -129,24 +110,23 @@ void needleValve_reset(int needleValveNo){
 
 }
 
-//****************************
-//Fuction for taking readings
-//from the rain gauges
-//****************************
+//***********************************************************
+// Device - rain gauges
+//***********************************************************
 void read_raingauges(void){
     //Read Raingauge 1A
-  currentPulseStateRG1A = MachineControl_DigitalInputs.read(DIN_READ_CH_PIN_00);  //  Read Raingauge input 1A = DIN0
-  if (lastPulseStateRG1A == HIGH && currentPulseStateRG1A == LOW) {
-    pulseCountRG1A++;  // count falling edge
+  currentPulseStateRGA[0] = MachineControl_DigitalInputs.read(DIN_READ_CH_PIN_00);  //  Read Raingauge input 1A = DIN0
+  if (lastPulseStateRGA[0] == HIGH && currentPulseStateRGA[0] == LOW) {
+    pulseCountRGA[0]++;  // count falling edge
   }
-  lastPulseStateRG1A = currentPulseStateRG1A;
+  lastPulseStateRGA[0] = currentPulseStateRGA[0];
   
   //Read Raingauge 1B
-  currentPulseStateRG1B = MachineControl_DigitalInputs.read(DIN_READ_CH_PIN_01);  //  Read Raingauge input 1B = DIN1
-  if (lastPulseStateRG1B == HIGH && currentPulseStateRG1B == LOW) {
-    pulseCountRG1B++;  // count falling edge
+  currentPulseStateRGB[0] = MachineControl_DigitalInputs.read(DIN_READ_CH_PIN_01);  //  Read Raingauge input 1B = DIN1
+  if (lastPulseStateRGB[0] == HIGH && currentPulseStateRGB[0] == LOW) {
+    pulseCountRGB[0]++;  // count falling edge
   }
-  lastPulseStateRG1B = currentPulseStateRG1B;
+  lastPulseStateRGB[0] = currentPulseStateRGB[0];
 /*
   //Read Raingauge 2A
   currentPulseStateRG2A = MachineControl_DigitalInputs.read(DIN_READ_CH_PIN_02);  //  Read Raingauge input 2A = DIN2
@@ -193,9 +173,9 @@ void read_raingauges(void){
   */
 }
 
-//****************************
+//***********************************************************
 //Test serial outputs
-//****************************
+//***********************************************************
 void serial_debug(void){
   // Re-check Serial every second
   if (millis() - lastSerialCheck > 1000) {
@@ -207,9 +187,9 @@ void serial_debug(void){
     Serial.print("NV1: ");
     Serial.print(voltageNV[0]);
     Serial.print(" V, RG1A: ");
-    Serial.print(pulseCountRG1A);
+    Serial.print(pulseCountRGA[0]);
     Serial.print(" , RG1B: ");
-    Serial.println(pulseCountRG1B);
+    Serial.println(pulseCountRGB[0]);
 /*
     Serial.print("NV2: ");
     Serial.print(voltageNV2);
@@ -235,9 +215,9 @@ void serial_debug(void){
   }
 }
 
-//****************************
-//Device heatbeat status
-//****************************
+//***********************************************************
+//Device - Heatbeat Status
+//***********************************************************
 void led_heartbeat(void){
     // Heartbeat LED: toggle every 500 ms
   if (millis() - lastHeartbeat > 500) {
@@ -246,7 +226,9 @@ void led_heartbeat(void){
     lastHeartbeat = millis();
   }
 }
-
+//***********************************************************
+//Device - Load Cell Controls
+//***********************************************************
 bool readLoadCells() {
   WeightPacket pkt = {0};
 
@@ -299,77 +281,10 @@ bool readLoadCells() {
   return true;
 }
 
-
-//*****************************************************************************************************
-//SETUP
-//*****************************************************************************************************
-void setup() {
-  // Initialize USB Serial
-  Serial.begin(115200);
-  delay(1000);   // allow USB to settle
-  serialActive = Serial;  //Serial comms watchdog to prevent lock up
-
-  Wire.begin();
-
-  if (serialActive) {
-    Serial.println("*********EML Calibrator*********");
-  }
-
-  //Set over current behavior of all solenoid digital channels to latch mode (true)
-  MachineControl_DigitalOutputs.begin(true);
-  //At startup set all solenoid digital channels to OPEN
-  MachineControl_DigitalOutputs.writeAll(0);
-
-  Wire.begin();
-
-  if (!MachineControl_DigitalProgrammables.begin()) {
-    Serial.println("GPIO expander initialization fail!!");
-  }
-
-  if (!MachineControl_DigitalInputs.begin()) {
-    Serial.println("Digital input GPIO expander initialization fail!!");
- }
-
-  // Initialize analog outputs - Needle Valves
-  MachineControl_AnalogOut.begin();
-  MachineControl_AnalogOut.setPeriod(NEEDLE_VALVE_1, PERIOD_MS);
-//  MachineControl_AnalogOut.setPeriod(NEEDLE_VALVE_2, PERIOD_MS);
-//  MachineControl_AnalogOut.setPeriod(NEEDLE_VALVE_3, PERIOD_MS);
-//  MachineControl_AnalogOut.setPeriod(NEEDLE_VALVE_4, PERIOD_MS);
-
-  // Initialize digtal outputs - Solenoids
-  solenoid_controller(SOLENOID_1, LOW);  // Turn off solenoid 1
-//  solenoid_controller(SOLENOID_2, LOW);  // Turn off solenoid 2
-//  solenoid_controller(SOLENOID_3, LOW);  // Turn off solenoid 3
-//  solenoid_controller(SOLENOID_4, LOW);  // Turn off solenoid 4
-
-  delay(1000);  // allow setups to settle 
-
-  Serial.println("Enter a number to choose a mode:");
-  Serial.println("1. IDLE");
-  Serial.println("2. SETUP");
-  Serial.println("3. BALANCE");
-  Serial.println("4. TEST");  
-  Serial.println("5. ERROR");
-  Serial.println("6. SHUTDOWN");
-
-}
-
-void printMenu() {
-  Serial.println("\nEnter a number to choose a mode:");
-  Serial.println("1. IDLE");
-  Serial.println("2. SETUP");
-  Serial.println("3. BALANCE");
-  Serial.println("4. TEST");
-  Serial.println("5. ERROR");
-  Serial.println("6. SHUTDOWN");
-  Serial.println("0. Exit current mode / return to menu");
-}
-
-//*****************************************************************************************************
-//LOOP
-//*****************************************************************************************************
-void loop() {
+//***********************************************************
+//  Read the selected MODE from serial
+//***********************************************************
+void readSelectedSerialMode(void){
 
   // Always check serial input
   if (Serial.available() > 0) {
@@ -381,37 +296,37 @@ void loop() {
     }
 
     switch (cmd) {
-      case '1':
+      case '0':
         mode = MODE_IDLE;
         Serial.println("System is in IDLE mode.");
         printMenu();
         break;
 
-      case '2':
+      case '1':
         mode = MODE_SETUP;
         Serial.println("System is in SETUP.");
         Serial.println("Press 0 to return to menu.");
         break;
 
-      case '3':
+      case '2':
         mode = MODE_BALANCE;
         Serial.println("System is in BALANCE TEST.");
         printMenu();
         break;
 
-      case '4':
+      case '3':
         mode = MODE_TEST;
         Serial.println("System is in mm/hr TEST.");
         printMenu();
         break;
 
-      case '5':
+      case '4':
         mode = MODE_ERROR;
         Serial.println("System encountered an ERROR!");
         printMenu();
         break;
 
-      case '6':
+      case '5':
         mode = MODE_SHUTDOWN;
         Serial.println("System is SHUTTING DOWN.");
         while (true) {
@@ -419,10 +334,10 @@ void loop() {
         }
         break;
 
-      case '0':
-          mode = MODE_IDLE;
-          printMenu();
-        break;
+//      case '0':
+//          mode = MODE_IDLE;
+//          printMenu();
+//        break;
 
       default:
         Serial.println("Invalid choice!");
@@ -430,7 +345,11 @@ void loop() {
         break;
     }
   }
-
+}
+//***********************************************************
+//  Run the selected MODE
+//***********************************************************
+void runActiveMode(void){
   // Run active mode
   switch (mode) {
 
@@ -467,6 +386,58 @@ void loop() {
     default:
       break;
   }
+}
 
+//*****************************************************************************************************
+//SETUP
+//*****************************************************************************************************
+void setup() {
+  // Initialize USB Serial
+  Serial.begin(115200);
+  delay(1000);   // allow USB to settle
+  serialActive = Serial;  //Serial comms watchdog to prevent lock up
+
+  Wire.begin();
+
+  if (serialActive) {
+    Serial.println("*********EML Calibrator*********");
+  }
+
+  //Set over current behavior of all solenoid digital channels to latch mode (true)
+  MachineControl_DigitalOutputs.begin(true);
+  //At startup set all solenoid digital channels to OPEN
+  MachineControl_DigitalOutputs.writeAll(0);
+
+  Wire.begin();
+
+  if (!MachineControl_DigitalProgrammables.begin()) {
+    Serial.println("GPIO expander initialization fail!!");
+  }
+
+  if (!MachineControl_DigitalInputs.begin()) {
+    Serial.println("Digital input GPIO expander initialization fail!!");
+ }
+
+  // Initialize analog outputs - Needle Valves
+  MachineControl_AnalogOut.begin();
+  for (int nv = 0; nv < 4; nv++) {
+    MachineControl_AnalogOut.setPeriod(nv, PERIOD_MS);  // Set period (ms)
+  }
+  // Initialize digtal outputs - Solenoids
+  for (int sd = 0; sd < 4; sd++) {
+    solenoid_controller(sd, LOW);  // Turn off solenoid 1 to 4
+  }
+
+  delay(1000);  // allow setups to settle 
+  printMenu();
+}
+
+//*****************************************************************************************************
+//LOOP
+//*****************************************************************************************************
+void loop() {
+
+  readSelectedSerialMode();
+  runActiveMode();
   delay(10);
 }
